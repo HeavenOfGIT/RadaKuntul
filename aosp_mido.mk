a656e711c9d329fad2a9ed04dcad0fe01ecceeda
RasyidAlKautsar <93345864+ArkanMuhammad1986@users.noreply.github.com>
2021-12-04 03:53:44 +0700

    mido: Update Build FP from Raven

--- aosp_mido.mk
+++ aosp_mido.mk
@@ -43,4 +43,4 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="mido-user 7.0 NRD90M V9.6.1.0.NCFMIFD release-keys"
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := "xiaomi/mido/mido:7.0/NRD90M/V9.6.1.0.NCFMIFD:user/release-keys"
+BUILD_FINGERPRINT := "google/raven/raven:12/SD1A.210817.036/7805805:user/release-keys"
