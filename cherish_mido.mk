813e6122448fdfbc9c8cba693c34257ebfd01803
RasyidAlKautsar <93345864+ArkanMuhammad1986@users.noreply.github.com>
2021-12-05 13:44:37 +0700

    mido: switch build FingerPrint To Redfin 12

--- cherish_mido.mk
+++ cherish_mido.mk
@@ -41,7 +41,7 @@ BOARD_VENDOR := Xiaomi
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="mido-user 7.0 NRD90M V9.6.1.0.NCFMIFD release-keys"
+    PRIVATE_BUILD_DESC="redfin-user 12 SP1A.211105.003 7757856 release-keys"
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := "google/raven/raven:12/SD1A.210817.036/7805805:user/release-keys"
+BUILD_FINGERPRINT := "google/redfin/redfin:12/SP1A.211105.003/7757856:user/release-keys"
